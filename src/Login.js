089236ea30df4a046cec3a20ca8530adbe509513
Mohd Ali <mohd.ali999@gmail.com>
2020-11-17 12:28:17 +0530

    chenges

--- src/Login.js
+++ src/Login.js
@@ -1,36 +1,44 @@
-import React, { Component, createContext } from 'react'
+import React, { Component } from 'react'
 import './Login.css'
 
 export default class Login extends Component {
 
     constructor(props){
     super(props);
-    this.data=new Map();
-    this.data.set('9813646722', '123456'); //Dummy data
-    const storedNumber=localStorage.getItem("number"); //Get number from browser storage
 
+    this.data=new Map();
+    //Dummy data
+    this.data.set('9813646722', '123456'); 
+    //Get number from browser storage
+    const storedNumber=localStorage.getItem("number"); 
+    //Set State   
     this.state={
       isLoggedIn:false,
       renderView:(storedNumber===null)?'':'LoggedIn',
-      number:(storedNumber===null)?'':storedNumber,
-      password:'',
-      cPassword:''      
     };
   }
   handleCheckNumber=(e)=>{
-    console.log("Number is: "+this.state.number);
+    console.log("Number is: "+this.number);
     var pattern = new RegExp("[0-9]{10}");
-    if(pattern.test(this.state.number)){
+    if(pattern.test(this.number)){
       //Check if number found 
-      if(this.data.has(this.state.number)){
+      /*Post API to Check the number the passowrd
+        curl -X POST \
+        /api/account/CheckNumber \
+        -H 'api_key: xyz' \
+        -H 'auth: Mzg2NixnblQ2ZnZMTEhiZDNzeDdacGxxczY1OA' \
+        -H 'cache-control: no-cache' \
+        -d 'number=this.number'
+      */
+      if(this.data.has(this.number)){
           console.log("Number found in DB");
           //Number found in database... Display Password authentication screen.
           this.setState({
-            renderView:'loginPassword'
+            renderView:'authPassword'
           });
       }else{
           console.log("Number not found in DB");
-          //Number found in database... Display screen to save the password
+          //Number not found in database... Create new account
           this.setState({
             renderView:'setPassword'
           });        
@@ -42,7 +50,15 @@ export default class Login extends Component {
   handleLogin=(e)=>{
     console.log("Login Button submit called");
     //Check password in data object
-    if(this.data.get(this.state.number)===this.state.password){
+    /*Post API to validate login with numbner and password
+      curl -X POST \
+      /api/account/login \
+      -H 'api_key: xyz' \
+      -H 'auth: Mzg2NixnblQ2ZnZMTEhiZDNzeDdacGxxczY1OA' \
+      -H 'cache-control: no-cache' \
+      -d 'number=this.number&password=this.password'
+    */
+    if(this.data.get(this.number)===this.password){
         localStorage.setItem("number", this.state.number);
         this.setState({
             renderView:'LoggedIn'
@@ -54,15 +70,27 @@ export default class Login extends Component {
   }
   handleSavePassword=(e)=>{
     console.log("Save Password Button submit called");
-    this.data.set(this.state.number, this.state.password)
+    /*Post API to create new account
+    curl -X POST \
+      /api/account/create \
+      -H 'api_key: xyz' \
+      -H 'auth: Mzg2NixnblQ2ZnZMTEhiZDNzeDdacGxxczY1OA' \
+      -H 'cache-control: no-cache' \
+      -d 'number=this.number&password=this.number'
+    */
+    this.data.set(this.number, this.password)
     this.setState({
         renderView:'LoggedIn'
     });
+    //Login user after creating the account. redirect user to logged in screen
     e.preventDefault();
   }
   logOut=()=>{
     localStorage.removeItem("number");
     console.log("Logout");
+    /*Post API to Logout
+    
+    */
     this.setState({
         number:"",
         renderView:""
@@ -70,16 +98,16 @@ export default class Login extends Component {
   }
   render(){
     switch (this.state.renderView) {
-      case'loginPassword':  
+      case'authPassword':  
         return (
           <div className="login">
             <h1>Login Screen</h1>
-            <div className="msg success">Number {this.state.number} is registerd with us. Enter password to login.</div>  
+            <div className="msg success">Number {this.number} is registerd with us. Enter password to login.</div>  
             <form method="post">
                 <input
                   key="loginPassword" 
                   type="password"
-                  onChange = {(event,newValue) => this.setState({password:event.target.value})}
+                  onChange = {(event,newValue) => this.password=event.target.value}
                   minLength="6" 
                   maxLength="10" 
                   placeholder="Enter password" 
@@ -93,13 +121,13 @@ export default class Login extends Component {
          return (
           <div className="login">
             <h1>Register</h1>
-         <div className="msg error">Opss..{this.state.number} is not registerd. Enter new password to get registerd.</div> 
+         <div className="msg error">Opss..{this.number} is not registerd. Enter new password to get registerd.</div> 
             <form method="post">
                 <div>
                     <input 
                     key="savePassword"
                     type="password"
-                    onChange = {(event,newValue) => this.setState({password:event.target.value})}
+                    onChange = {(event,newValue) => this.password=event.target.value}
                     minLength="6"
                     maxLength="10" 
                     placeholder="Enter new password" 
@@ -115,7 +143,7 @@ export default class Login extends Component {
       case'LoggedIn':
       return(
         <div className="login">
-            <h1> Hello {this.state.number}</h1>
+            <h1> Hello {this.number}</h1>
             <div>
                 <button onClick={this.logOut}>LogOut</button>
             </div>
@@ -131,7 +159,7 @@ export default class Login extends Component {
                   <input 
                     key="loginInput"
                     type="tel" 
-                    onChange = {(event,newValue) => {this.setState({number:event.target.value})}}
+                    onChange = {(event,newValue) => this.number=event.target.value}
                     minLength="10"
                     maxLength="10"
                     placeholder="Enter Phone Number" 
